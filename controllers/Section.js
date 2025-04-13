7f2ce5de3f27cbf9ef9d77ed54e264bb6ec238a5
priyanshu singh <ishusingh4006@gmail.com>
2025-04-13 17:59:58 +0530

    Add Section.js controller and add CRUD operation handler

---
+++ controllers/Section.js
@@ -0,0 +1,102 @@
+const Section=require("../models/Section");
+const Course=require("../models/Course");
+
+exports.createSection=async (req,res)=>{
+    try {
+        //fetch the data
+        const {sectionName,courseId}=req.body;
+        //data validation
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success:false,
+                message:"All field Required",
+            });
+        }
+        //create section
+        const newSection=await Section.create({sectionName});
+        //update course with section objectId
+        const updatedCourseDetails=await Course.findByIdAndUpdate(
+                                            courseId,
+                                            {
+                                                $push:{
+                                                    courseContent:newSection._id,
+                                                }
+                                            },
+                                            {new:true}
+                                        )
+                                        .populate({
+                                            path:"courseContent",
+                                            populate:{
+                                                path:"subSection",
+                                            }
+                                        });
+        return res.status(200).json({
+            success:true,
+            message:"section created Successfully",
+            updatedCourseDetails,
+        });
+         
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Unable to create section,please try again",
+            error:error.message
+        });
+    }
+}
+
+//section ko update karna hai
+exports.updateSection=async (req,res)=>{
+    try {
+        //data input
+        const {sectionName,sectionId}=req.body;
+        //data validation
+        if(!sectionName || !sectionId){
+            return res.status(401).json({
+                success:false,
+                message:"All field required",
+            })
+        }
+        //update data
+        const section=await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true });
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"section updated successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Unable to update section,please try again",
+            error:error.message
+        });
+    }
+};
+
+//delete the section
+exports.deleteSection=async (req,res)=>{
+    try {
+        //fetch the data // get ID-->assuming that we are sending Id in params
+        const {sectionId}=req.params;
+        //validate 
+        if(!sectionId){
+            return res.status(401).json({
+                success:false,
+                message:"All field required",
+            })
+        }
+        //delete the section
+        await Section.findByIdAndDelete(sectionId,{new:true});
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"section deleted successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:"Unable to delete section,please try again",
+            error:error.message
+        });
+    }
+}
