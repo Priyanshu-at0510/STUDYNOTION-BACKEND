f08d2fd681cf202aab8cf9d15e54efca44c81721
priyanshu singh <ishusingh4006@gmail.com>
2025-05-13 18:03:58 +0530

    Update RatingAndReview controller ,adding getAllReviews function on it

--- controllers/RatingAndReview.js
+++ controllers/RatingAndReview.js
@@ -107,4 +107,32 @@ exports.getAverageRating=async (req,res)=>{
 }
 
 
-//getAllRating
+//getAllRating and reviews
+exports.getAllRating=async (req,res)=>{
+    try {
+        const allReviews=await RatingAndReview.find({})
+                                .sort({rating:"desc"})
+                                .populate({
+                                    path:"user",
+                                    select:"firstName lastName email image",
+                                })
+                                .populate({
+                                    path:"course",
+                                    select:"courseName"
+                                })
+                                .exec()
+        return res.status(200).json({
+            success:true,
+            message:"all reviews fetched successfully",
+            data:allReviews,
+        });
+    
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
